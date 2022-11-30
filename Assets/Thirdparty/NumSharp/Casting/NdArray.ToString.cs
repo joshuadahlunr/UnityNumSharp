a9d23d5d8b202b4e12b99fb679fb25fa1bc3d5d4
Joshua Dahl <joshuadahl@nevada.unr.edu>
2022-11-30 13:06:19 -0800

    Add NumSharp.Lite

---
+++ Assets/Thirdparty/NumSharp/Casting/NdArray.ToString.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using NumSharp.Backends;
+
+namespace NumSharp
+{
+    public partial class NDArray
+    {
+        public override string ToString()
+        {
+            return ToString(flat: false);
+        }
+
+        public string ToString(bool flat)
+        {
+            var s = new StringBuilder();
+            if (shape.Length == 0)
+            {
+                s.Append($"{Storage.GetAtIndex(0)}");
+            }
+            else if (shape.Length == 1 && Type.GetTypeCode(dtype) == TypeCode.Char)
+            {
+                s.Append(string.Join("", GetData<char>()));
+            }
+            else
+            {
+                if (flat)
+                    s.Append("array(");
+                PrettyPrint(s, flat);
+                if (flat)
+                    s.Append(")");
+            }
+
+            return s.ToString();
+        }
+
+        private void PrettyPrint(StringBuilder s, bool flat = false)
+        {
+            if (Shape.IsScalar)
+            {
+                s.Append($"{Storage.GetAtIndex(0)}");
+                return;
+            }
+
+            if (shape.Length == 1)
+            {
+                s.Append("[");
+                if (typecode == NPTypeCode.Char)
+                {
+                    s.Append("\"");
+                    s.Append(string.Join("", this.AsIterator<char>()));
+                    s.Append("\"");
+                }
+                else
+                {
+                    var items = this.AsIterator().Cast<object>();
+                    if (this.size <= 10)
+                        s.Append(string.Join(", ", items.Select(v => v.ToString())));
+                    else
+                    {
+                        s.Append(string.Join(", ", items.Take(5).Select(v => v.ToString())));
+                        s.Append(", ..., ");
+                        s.Append(string.Join(", ", items.Skip(this.size - 10).Select(v => v.ToString())));
+                    }
+                }
+
+                s.Append("]");
+                return;
+            }
+
+            var size = shape[0];
+            s.Append("[");
+
+            if (size <= 10)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    var n_minus_one_dim_slice = this[i];
+                    n_minus_one_dim_slice.PrettyPrint(s, flat);
+                    if (i < size - 1)
+                    {
+                        s.Append(", ");
+                        if (!flat)
+                            s.AppendLine();
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    var n_minus_one_dim_slice = this[i];
+                    n_minus_one_dim_slice.PrettyPrint(s, flat);
+                    if (i < size - 1)
+                    {
+                        s.Append(", ");
+                        if (!flat)
+                            s.AppendLine();
+                    }
+                }
+
+                s.Append(" ... ");
+                s.AppendLine();
+
+                for (int i = size - 5; i < size; i++)
+                {
+                    var n_minus_one_dim_slice = this[i];
+                    n_minus_one_dim_slice.PrettyPrint(s, flat);
+                    if (i < size - 1)
+                    {
+                        s.Append(", ");
+                        if (!flat)
+                            s.AppendLine();
+                    }
+                }
+            }
+
+            s.Append("]");
+        }
+    }
+}
---
+++ Assets/Thirdparty/NumSharp/Creation/np.frombuffer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using NumSharp.Utilities;
+
+namespace NumSharp
+{
+    public static partial class np
+    {
+        public static NDArray frombuffer(byte[] bytes, Type dtype)
+        {
+
+            //TODO! all types
+            if (dtype.Name == "Int32")
+            {
+                var size = bytes.Length / InfoOf<int>.Size;
+                var ints = new int[size];
+                for (var index = 0; index < size; index++)
+                {
+                    ints[index] = BitConverter.ToInt32(bytes, index * InfoOf<int>.Size);
+                }
+
+                return new NDArray(ints);
+            }
+            else if (dtype.Name == "Single")
+            {
+                var size = bytes.Length / InfoOf<float>.Size;
+                var floats = new float[size];
+                for (var index = 0; index < size; index++)
+                {
+                    floats[index] = BitConverter.ToSingle(bytes, index * InfoOf<float>.Size);
+                }
+
+                return new NDArray(floats);
+            }
+            else if (dtype.Name == "Double")
+            {
+                var size = bytes.Length / InfoOf<double>.Size;
+                var doubles = new double[size];
+                for (var index = 0; index < size; index++)
+                {
+                    doubles[index] = BitConverter.ToDouble(bytes, index * InfoOf<double>.Size);
+                }
+
+                return new NDArray(doubles);
+            }
+            else if (dtype.Name == "Byte")
+            {
+                var size = bytes.Length / InfoOf<byte>.Size;
+                var ints = bytes;
+                return new NDArray(bytes);
+            }
+
+            throw new NotImplementedException("");
+        }
+
+        public static NDArray frombuffer(byte[] bytes, string dtype)
+        {
+            if (dtype == ">u4")
+            {
+                var size = bytes.Length / InfoOf<int>.Size;
+                var ints = new int[size];
+                for (var index = 0; index < size; index++)
+                    ints[index] = bytes[0] * 256 + bytes[1] + bytes[2] * 256 + bytes[3];
+
+                return new NDArray(ints);
+            }
+
+            throw new NotImplementedException("");
+        }
+    }
+}
